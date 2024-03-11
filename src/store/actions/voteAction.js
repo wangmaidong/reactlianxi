11cac79905c3c9d4b5d2e4cd6a2d0c73a0e24425
wanglei <2934490605@qq.com>
2024-03-11 16:57:39 +0800

    feat: 中间件使用

--- src/store/actions/voteAction.js
+++ src/store/actions/voteAction.js
@@ -1,11 +1,20 @@
 import { VOTE_OPP, VOTE_SUP } from "../action-types";
+function wait (time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, time)
+    })
+}
 const voteAction = {
-    support() {
-        return {
-            type: VOTE_SUP
+     support() {
+        return async dispatch => {
+            await wait(3* 1000)
+            return dispatch({
+                type: VOTE_SUP
+            })
         }
     },
-    oppose() {
+    async oppose() {
+        await wait(2* 1000)
         return {
             type: VOTE_OPP
         }
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from './reducers/index.js'
- const store = createStore(reducer)
+import reduxLogger from 'redux-logger'
+import reduxThunk from 'redux-thunk'
+import reduxPromise from 'redux-promise'
+ const store = createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise))
  export default store
